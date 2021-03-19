af8f2ac210553936e2268c1b1acbed3d8c20810d
Sandy Ponticel <sponticel@gmail.com>
2021-03-18 20:16:33 -0400

    using find method

--- get-productive/src/components/TaskForm.jsx
+++ get-productive/src/components/TaskForm.jsx
@@ -1,11 +1,19 @@
 // Source used for drag and drop feature: https://www.freecodecamp.org/news/how-to-add-drag-and-drop-in-react-with-react-beautiful-dnd/#what-is-drag-and-drop
-import React from "react"
+import React from "react";
+import { useParams } from "react-router-dom";
+
 
 
 function Taskform(props) {
-  const {task,date} = props.task.fields;
+  const params = useParams();
+  const taskList = props.labels.find((label) => params.name === label.task);
+  if (!taskList) {
+    return <h2> Loading content ...</h2>
+  }
+
+  // const {task,date} = props.task.fields;
   return (
-    <div>
+    <div className="main">
       <h3>{task}</h3>
       <h4>{date}</h4>
     </div>
